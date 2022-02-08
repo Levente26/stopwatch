32c0fc8d93b137fbd72ef15e539c9b4730f9ea8b
Levente26 <madi.levente26@gmail.com>
2022-02-08 08:35:14 +0100

    timer list

--- src/components/timerlist/TimerList.jsx
+++ src/components/timerlist/TimerList.jsx
@@ -1,13 +1,20 @@
 import './TimerList.scss'
-
+import { Link } from 'react-router-dom'
 
 const TimerList = ({ timers }) => {
 
     console.log(timers)
 
     return (
-        <div>
-
+        <div className='timer-list'>
+            {timers.map(timer => (
+                <Link to={`/timer/${timer.id}`} className='p-1 m-1'>
+                    <h4>{timer.name}</h4>
+                    <p>{timer.details}</p>
+                    <p>Short break time: {timer.shortBreak} minutes</p>
+                    <p>Long break time: {timer.longBreak} minutes</p>
+                </Link>
+            ))}
         </div>
     )
 }
